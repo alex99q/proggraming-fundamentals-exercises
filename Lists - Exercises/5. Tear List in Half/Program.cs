9b93819ce6009cd568ca7af6a4dccd0aaaab0b1c
alex99q <alex99q@abv.bg>
2017-02-21 11:50:36 +0200

    Added some more exercise programs

--- Lists - Exercises/5. Tear List in Half/Program.cs
+++ Lists - Exercises/5. Tear List in Half/Program.cs
@@ -1,16 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _5.Tear_List_in_Half
+﻿namespace _5.Tear_List_in_Half
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
     class Program
     {
         static void Main(string[] args)
         {
             List<int> input = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            List<int> result = new List<int>();
+
+            for (int i = 0, j = input.Count / 2; i < input.Count / 2; i++, j++)
+            {
+                    int currentLeftNumber = input[i];
+                    int currentRightNumber = input[j];
+
+                    result.Add(currentRightNumber / 10);
+                    result.Add(currentLeftNumber);
+                    result.Add(currentRightNumber % 10);
+            }
+
+            Console.WriteLine(string.Join(" ", result));
         }
     }
 }
