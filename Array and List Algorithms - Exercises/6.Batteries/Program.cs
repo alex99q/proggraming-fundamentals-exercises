9c98e163d3235d3c68f67ea4afd5cfe8d5c55b05
alex99q <alex99q@abv.bg>
2017-02-28 15:02:28 +0200

    Added some more exercise programs

---
+++ Array and List Algorithms - Exercises/6.Batteries/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace Batteries
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			double[] capacity = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+			double[] usagePerHour = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+			int hoursTested = int.Parse(Console.ReadLine());
+
+			for (int i = 0; i < capacity.Length; i++)
+			{
+				double currentCapacity = capacity[i];
+				double currentUsage = usagePerHour[i];
+				int hoursLasted = 0;
+
+				for (int j = 0; j < hoursTested; j++)
+				{
+					if (currentCapacity > 0)
+					{
+						hoursLasted++;
+					}
+
+					currentCapacity -= currentUsage;
+				}
+
+				double percentage = (currentCapacity / capacity[i]) * 100;
+
+				if (currentCapacity < 0)
+				{
+					Console.WriteLine($"Battery {i + 1}: dead (lasted {hoursLasted} hours)");
+				}
+				else 
+				{
+					Console.WriteLine($"Battery {i + 1}: {currentCapacity:F2} mAh ({percentage:F2})%");
+				}
+			}
+		}
+	}
+}
