085e2da27665d680a5ce4b0ffe6cb6d5eb280fc1
alex99q <alex99q@abv.bg>
2017-03-07 11:30:13 +0200

    Added some more exercise programs

---
+++ Dictionaries - Exercises/1. Letter Repetition/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Letter_Repetition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Dictionary<char, int> lettersCount = new Dictionary<char, int>();
+
+            foreach (var letter in input)
+            {
+                if (!lettersCount.ContainsKey(letter))
+                {
+                    lettersCount[letter] = 0;
+                }
+
+                lettersCount[letter]++;
+            }
+
+            foreach (KeyValuePair<char, int> kvp in lettersCount)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
