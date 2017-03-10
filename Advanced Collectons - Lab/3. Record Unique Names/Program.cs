9edeb5e7ee99a040a7bce200fe48aec1e166cbeb
alex99q <alex99q@abv.bg>
2017-03-10 21:57:45 +0200

    Added some more exercise programs

---
+++ Advanced Collectons - Lab/3. Record Unique Names/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3.Record_Unique_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> names = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                names.Add(input);
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, names));
+        }
+    }
+}
---
+++ Advanced Collectons - Lab/4. Group Continents, Countries and Cities/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4.Group_Continents__Countries_and_Cities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var towns = new SortedDictionary<string, SortedDictionary<string, SortedSet<string>>>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] inputTokens = Console.ReadLine().Split();
+
+                string continent = inputTokens[0];
+                string country = inputTokens[1];
+                string town = inputTokens[2];
+
+                if (!towns.ContainsKey(continent))
+                {
+                    towns[continent] = new SortedDictionary<string, SortedSet<string>>();
+                }
+
+                if (!towns[continent].ContainsKey(country))
+                {
+                    towns[continent][country] = new SortedSet<string>();
+                }
+
+                towns[continent][country].Add(town);
+            }
+
+            foreach (var ContinentCountryPair in towns)
+            {
+                string continent = ContinentCountryPair.Key;
+                var countryTownsDict = ContinentCountryPair.Value;
+                Console.WriteLine($"{continent}:");
+
+                foreach (var countryTownsPair in countryTownsDict)
+                {
+                    string country = countryTownsPair.Key;
+                    var countryTowns = countryTownsPair.Value;
+
+                    Console.WriteLine("  {0} -> {1}", country, string.Join(", ", countryTowns));
+                }
+            }
+        }
+    }
+}
