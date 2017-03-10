4e7f24dead08df10a668e7fc78be190116133449
alex99q <alex99q@abv.bg>
2017-03-10 11:50:37 +0200

    Added some more exercise programs

---
+++ Advanced Collectons - Lab/1. Average Student Grades/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1.Average_Student_Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<double>> nameGradesDict = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                string name = input[0];
+                double grade = double.Parse(input[1]);
+
+                if (!nameGradesDict.ContainsKey(name))
+                {
+                    nameGradesDict[name] = new List<double>();
+                }
+                nameGradesDict[name].Add(grade);
+            }
+
+            foreach (var nameGradesPair in nameGradesDict)
+            {
+                string name = nameGradesPair.Key;
+                List<double> grades = nameGradesPair.Value;
+                double average = grades.Average();
+
+                Console.Write($"{name} -> ");
+
+                foreach (var grade in grades)
+                    Console.Write($"{grade:F2} ");
+
+                Console.WriteLine($"(avg: {average:F2})");
+            }
+        }
+    }
+}
---
+++ Advanced Collectons - Lab/2. Cities by Continent and Country/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2.Cities_by_Continent_and_Country
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var towns = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] inputTokens = Console.ReadLine().Split();
+
+                string continent = inputTokens[0];
+                string country = inputTokens[1];
+                string town = inputTokens[2];
+
+                AddTown(towns, continent, country, town);
+            }
+
+            foreach (var continentCountryTownsPair in towns)
+            {
+                string continent = continentCountryTownsPair.Key;
+                Console.WriteLine($"{continent}:");
+
+                var countryTownsDict = continentCountryTownsPair.Value;
+                foreach (var countryTownsPair in countryTownsDict)
+                {
+                    string country = countryTownsPair.Key;
+                    var countryTowns = countryTownsPair.Value;
+
+                    Console.WriteLine("  {0} -> {1}", country, string.Join(", ", countryTowns));
+                }
+            }
+
+        }
+
+        private static void AddTown(Dictionary<string, Dictionary<string, List<string>>> towns, string continent, string country, string town)
+        {
+            if (!towns.ContainsKey(continent))
+            {
+                towns[continent] = new Dictionary<string, List<string>>();
+            }
+
+            if (!towns[continent].ContainsKey(country))
+            {
+                towns[continent][country] = new List<string>();
+            }
+
+            towns[continent][country].Add(town);
+        }
+    }
+}
