fa9b1baea89dc5f9ff41faab39fddb5118c530c7
alex99q <alex99q@abv.bg>
2017-03-15 11:26:22 +0200

    Added some more exercise programs

--- LambdaAndLINQExercises/1.RegisterUsers/Program.cs
+++ LambdaAndLINQExercises/1.RegisterUsers/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 
 namespace RegisterUsers
@@ -10,17 +11,32 @@ namespace RegisterUsers
 		{
 			string input = Console.ReadLine();
 
-			Dictionary<string, DateTime> date = new Dictionary<string, DateTime>();
+			Dictionary<string, DateTime> usernameRegistrationDate = new Dictionary<string, DateTime>();
 
 			while (input != "end")
 			{
 				string[] inputTokens = input.Split("-> ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-				date[inputTokens[0]] = DateTime.Parse(inputTokens[1]);
+
+                string username = inputTokens[0];
+                string date = inputTokens[1];
+
+				usernameRegistrationDate[username] = DateTime.ParseExact(date, "dd/mm/yyyy", CultureInfo.InvariantCulture);
 
 				input = Console.ReadLine();
 			}
 
-			date = date.Take(5).OrderBy(n => n).ToDictionary((KeyValuePair<string, DateTime> arg) => arg);
+			usernameRegistrationDate = 
+                usernameRegistrationDate
+                    .Reverse()
+                    .OrderBy(n => n.Value)
+                    .Take(5)
+                    .OrderByDescending(n => n.Value)
+                    .ToDictionary(n => n.Key, n => n.Value);
+
+            foreach (var username in usernameRegistrationDate.Keys)
+            {
+                Console.WriteLine(username);
+            }
 		}
 	}
 }
---
+++ LambdaAndLINQExercises/2. Default Values/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2.Default_Values
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Dictionary<string, string> inputValues = new Dictionary<string, string>();
+
+            while (input != "end")
+            {
+                string[] inputTokens = input.Split("-> ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+                string key = inputTokens[0];
+                string value = inputTokens[1];
+
+                inputValues[key] = value;
+
+                input = Console.ReadLine();
+            }
+
+            string defaultValue = Console.ReadLine();
+
+            Dictionary<string, string> unchangedValues = inputValues
+                .Where(n => n.Value != "null")
+                .OrderByDescending(n => n.Value.Length)
+                .ToDictionary(n => n.Key, n => n.Value);
+
+            Dictionary<string, string> changedValues = inputValues
+                .Where(n => n.Value == "null")
+                .ToDictionary(n => n.Key, n => defaultValue);
+
+            foreach (var kvp in unchangedValues)
+            {
+                Console.WriteLine("{0} <-> {1}", kvp.Key, kvp.Value);
+            }
+
+            foreach (var kvp in changedValues)
+            {
+                Console.WriteLine("{0} <-> {1}", kvp.Key, kvp.Value);
+            }
+        }
+    }
+}
