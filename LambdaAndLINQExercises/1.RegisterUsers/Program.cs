8a54ff2062e7ee01c571dade82570e5d20e9a64e
alex99q <alex99q@abv.bg>
2017-03-15 10:14:02 +0200

    Added some more exercise programs

---
+++ LambdaAndLINQExercises/1.RegisterUsers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RegisterUsers
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			string input = Console.ReadLine();
+
+			Dictionary<string, DateTime> date = new Dictionary<string, DateTime>();
+
+			while (input != "end")
+			{
+				string[] inputTokens = input.Split("-> ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+				date[inputTokens[0]] = DateTime.Parse(inputTokens[1]);
+
+				input = Console.ReadLine();
+			}
+
+			date = date.Take(5).OrderBy(n => n).ToDictionary((KeyValuePair<string, DateTime> arg) => arg);
+		}
+	}
+}
+	
