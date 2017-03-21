922922a528f7617ac9aba9bc4e782ade32000e53
alex99q <alex99q@abv.bg>
2017-03-21 21:15:21 +0200

    Added some more exercise programs

--- Lambda and LINQ - More Exercises/3. LINQuistics/Program.cs
+++ Lambda and LINQ - More Exercises/3. LINQuistics/Program.cs
@@ -1,91 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _03.LINQuistics
 {
-    class LINQuistics
-    {
-        static void Main(string[] args)
-        {
-            string inputLine = Console.ReadLine();
-            var collectionDict = new Dictionary<string, HashSet<string>>();
+	class LINQuistics
+	{
+		static void Main(string[] args)
+		{
+			string input = Console.ReadLine();
+			Dictionary<string, List<string>> collectionMethodsDict = new Dictionary<string, List<string>>();
 
-            while (!inputLine.Equals("exit"))
-            {
-                var dataLine = inputLine.Split(new char[] { '.', ')', '(' }, StringSplitOptions.RemoveEmptyEntries);
-                var collection = dataLine[0];
+			while (input != "end")
+			{
+				string[] inputTokens = input.Split(".()".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
 
-                if (inputLine.Contains("."))
-                { 
-                    if (!collectionDict.ContainsKey(collection))
-                    {
-                        collectionDict[collection] = new HashSet<string>();
-                    }
-                    for (int i = 1; i < dataLine.Length; i++)
-                    {
-                        collectionDict[collection].Add(dataLine[i]);
-                    }
-                }
-                else
-                {
-                    if (collectionDict.ContainsKey(collection))
-                    {
-                        int number = 0;
-                        if (Int32.TryParse(inputLine, out number))
-                        {
-                            foreach (var methodName in collectionDict.Values
-                                .OrderByDescending(x => x.Count()).First()
-                                .OrderBy(x => x.Length).Take(number))
-                            {
-                                Console.WriteLine($"* {methodName}");
-                            }
-                        }
-                        else if (collectionDict.ContainsKey(inputLine))
-                        {
-                            var result = collectionDict[inputLine]
-                                .OrderByDescending(x => x.Length)
-                                .ThenByDescending(x => x.Distinct().Count())
-                                .ToList();
-                            foreach (var methodName in result)
-                            {
-                                Console.WriteLine($"* {methodName}");
-                            }
-                        }
-                    }                
-                }
+				string collection = inputTokens[0];
 
-                inputLine = Console.ReadLine();
-            }
+				if (!collectionMethodsDict.ContainsKey(collection))
+				{
+					collectionMethodsDict[collection] = new List<string>();
+				}
 
-            string[] commandLine = Console.ReadLine().Split();
-            var method = commandLine[0];
-            var selection = commandLine[1];
+				for (int i = 1; i < inputTokens.Length; i++)
+				{
+					string method = inputTokens[i];
 
-            if (commandLine.Contains("all"))
-            {
-                foreach (var collection in collectionDict.Where(x => x.Value.Contains(method))
-                    .OrderByDescending(x => x.Value.Count())
-                    .ThenByDescending(x => x.Value.Min(y => y.Length)))
-                {
-                    Console.WriteLine(collection.Key);
-                    foreach (var methodName in collection.Value.OrderByDescending(x => x.Count()))
-                    {
-                        Console.WriteLine($"* {methodName}");
-                    }
-                }
-            }
-            else
-            {
-                foreach (var collection in collectionDict.Where(x => x.Value.Contains(method))
-                    .OrderBy(x => x.Key.Count())
-                    .ThenByDescending(x => x.Value.Min(y => y.Length)))
-                {
-                    Console.WriteLine(collection.Key);
-                }
-            }
-        }
-    }
+					collectionMethodsDict[collection].Add(method);
+				}
+
+				collectionMethodsDict[collection] = 
+					collectionMethodsDict[collection]
+						.Distinct()
+						.OrderByDescending(n => n.Length)
+						.ToList();
+
+				for (int i = 0; i < ; i++)
+				{
+
+				}
+
+				input = Console.ReadLine();
+			}
+		}
+	}
 }
