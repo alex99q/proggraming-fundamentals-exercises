c2853c9523503c02d44b1369db4eaa7f272b0248
alex99q <alex99q@abv.bg>
2017-03-28 19:20:37 +0300

    Added some more exercise files

---
+++ Files, Directories and Exceptions - Lab/4.Merge_Files/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Merge_Files
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			string[] input1 = File.ReadLines("../../../Resources/04.Merge Files/FileOne.txt").ToArray();
+			string[] input2 = File.ReadLines("../../../Resources/04.Merge Files/FileTwo.txt").ToArray();
+
+			string[] input = input1.Concat(input2).OrderBy(n => n).ToArray();
+		}
+	}
+}
