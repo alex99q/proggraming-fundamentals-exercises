a4f19cb207956eb9e86570ef7a253cbf228df2b6
alex99q <alex99q@abv.bg>
2017-04-01 18:10:48 +0300

    Added some more exercise programs

---
+++ Files, Directories and Exceptions - Lab/5. Folder Size/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+namespace _5.Folder_Size
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fileNames = Directory.GetFiles("../../../Resources/05. Folder Size/TestFolder");
+
+            double sumSize = 0;
+
+            foreach (var file in fileNames)
+            {
+                FileInfo currentFileInfo = new FileInfo(file);
+
+                sumSize += currentFileInfo.Length;
+            }
+
+            string sumSizeinMB = (sumSize / 1024 / 1024).ToString();
+
+            File.WriteAllText("../../../Resources/05. Folder Size/TestFolder/Size Of TestFolder.txt", sumSizeinMB);
+        }
+    }
+}
