ba027e0a86843c9e2ec0997dbb835738c9d7ff8b
alex99q <alex99q@abv.bg>
2017-04-01 18:10:01 +0300

    Added some more exercise programs

--- Files, Directories and Exceptions - Lab/4.Merge_Files/Program.cs
+++ Files, Directories and Exceptions - Lab/4.Merge_Files/Program.cs
@@ -1,17 +1,18 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 
 namespace Merge_Files
 {
-	class MainClass
+    class MainClass
 	{
 		public static void Main(string[] args)
 		{
-			string[] input1 = File.ReadLines("../../../Resources/04.Merge Files/FileOne.txt").ToArray();
-			string[] input2 = File.ReadLines("../../../Resources/04.Merge Files/FileTwo.txt").ToArray();
+			string[] input1 = File.ReadLines("../../../Resources/04. Merge Files/FileOne.txt").ToArray();
+			string[] input2 = File.ReadLines("../../../Resources/04. Merge Files/FileTwo.txt").ToArray();
 
-			string[] input = input1.Concat(input2).OrderBy(n => n).ToArray();
+			string[] input = input1.Concat(input2).OrderBy(n => int.Parse(n)).ToArray();
+
+            File.WriteAllLines("../../../Resources/04. Merge Files/Output.txt", input);
 		}
 	}
 }
