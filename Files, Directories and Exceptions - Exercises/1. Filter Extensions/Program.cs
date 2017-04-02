0ca7a236bd2ca816ebf09cda75e35888ff495c0c
alex99q <alex99q@abv.bg>
2017-04-02 12:35:12 +0300

    Added some more exercise programs

---
+++ Files, Directories and Exceptions - Exercises/1. Filter Extensions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _1.Filter_Extensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputExtention = Console.ReadLine();
+
+            var files = Directory.GetFiles("../../../Exercises-Resource/01. Filter-Extensions/input");
+
+            List<string> fileNames = new List<string>();
+
+            foreach (var file in files)
+            {
+                string currentExtention = file.Split('.').Reverse().Take(1).ToArray()[0];
+
+                if (currentExtention == inputExtention)
+                {
+                    fileNames.Add(file.Split('\\').Reverse().Take(1).ToArray()[0]);
+                }
+            }
+
+            File.WriteAllLines("../../../Exercises-Resource/01. Filter-Extensions/output.txt", fileNames);
+        }
+    }
+}
---
+++ Files, Directories and Exceptions - Exercises/2. HTML Contents/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace _2.HTML_Contents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputLines = File.ReadAllLines("../../Input.txt");
+
+            string title = string.Empty;
+            List<string> bodyParts = new List<string>();
+
+            foreach (var line in inputLines)
+            {
+                if (line == "exit")
+                {
+                    break;
+                }
+
+                string[] lineParts = line.Split(' ');
+
+                string tag = lineParts[0];
+                string content = lineParts[1];
+
+                if (tag == "title")
+                {
+                    title = "\t<title>" + content + "</title>";
+                }
+                else
+                {
+                    bodyParts.Add("<" + tag + ">" + content + "</" + tag + ">");
+                }
+            }
+
+            StringBuilder result = new StringBuilder();
+
+            result.AppendLine("<!DOCTYPE html>");
+            result.AppendLine("<html>");
+            result.AppendLine("<head>");
+
+            if (title != string.Empty)
+            {
+                result.AppendLine(title);
+            }
+
+            result.AppendLine("</head>");
+            result.AppendLine("<body>");
+
+            if (bodyParts.Any())
+            {
+                result.AppendLine(string.Join(Environment.NewLine, bodyParts));
+            }
+
+            result.AppendLine("</body>");
+            result.AppendLine("</html>");
+
+            File.WriteAllText("../../Index.html", result.ToString());
+        }
+    }
+}
