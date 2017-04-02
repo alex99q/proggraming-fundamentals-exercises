b042d5c6e23951e2b870b331cf7dbdd914e36c29
alex99q <alex99q@abv.bg>
2017-04-03 01:39:25 +0300

    Added some more exercise programs

---
+++ Files, Directories and Exceptions - Exercises/3. User Database/Program.cs
@@ -0,0 +1,121 @@
+﻿namespace _3.User_Database
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    public class Program
+    {
+        private static string databasePath = "Users.txt";
+        private static string loggedInUser = null;
+        private static Dictionary<string, string> users = new Dictionary<string, string>();
+
+
+        public static void Main(string[] args)
+        {
+            if (!File.Exists(databasePath))
+            {
+                File.Create(databasePath).Close();
+            }
+            else
+            {
+                foreach (var user in File.ReadAllLines(databasePath))
+                {
+                    var userParameters = user.Split(' ');
+
+                    string username = userParameters[0];
+                    string password = userParameters[1];
+
+                    users[username] = password;
+                }
+            }
+
+            string[] input = File.ReadAllLines("Input.txt");
+
+            foreach (var inputLine in input)
+            {
+                string[] commandParameters = inputLine.Split(' ');
+
+                string command = commandParameters[0];
+
+                if (command == "exit")
+                    break;
+
+                switch (command)
+                {
+                    case "register":
+                        string username = commandParameters[1];
+                        string password = commandParameters[2];
+                        string confirmPassword = commandParameters[3];
+                        Register(username, password, confirmPassword);
+                        break;
+
+                    case "login":
+                        username = commandParameters[1];
+                        password = commandParameters[2];
+                        Login(username, password);
+                        break;
+
+                    case "logout":
+                        Logout();
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private static void Logout()
+        {
+            if (loggedInUser == null)
+            {
+                Console.WriteLine("There is no currently logged in user.");
+                return;
+            }
+
+            loggedInUser = null;
+        }
+
+        private static void Login(string username, string password)
+        {
+            if (!users.ContainsKey(username))
+            {
+                Console.WriteLine("There is no user with the given username.");
+                return;
+            }
+
+            if (loggedInUser != null)
+            {
+                Console.WriteLine("There is already a logged in user.");
+                return;
+            }
+
+            if (users[username] != password)
+            {
+                Console.WriteLine("The password you entered is incorrect.");
+                return;
+            }
+
+            loggedInUser = username;
+        }
+
+        private static void Register(string username, string password, string confirmPassword)
+        {
+            if (users.ContainsKey(username))
+            {
+                Console.WriteLine("The given username already exists.");
+                return;
+            }
+
+            if (password != confirmPassword)
+            {
+                Console.WriteLine("The two passwords must match.");
+                return;
+            }
+
+            users[username] = password;
+            File.AppendAllText(databasePath, $"{username} {password}\r\n");
+        }
+    }
+}
---
+++ Files, Directories and Exceptions - Exercises/4. Re-Directory/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _4.Re_Directory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputDirectory = "../../../Exercises-Resource/04. Re-Directory/input";
+            string outputDirectory = "../../../Exercises-Resource/04. Re-Directory/output";
+
+            List<string> extensions = new List<string>();
+
+            string[] files = Directory.GetFiles(inputDirectory);
+
+            foreach (var file in files)
+            {
+                FileInfo currentFileInfo = new FileInfo(file);
+
+                extensions.Add(currentFileInfo.Extension.TrimStart('.'));
+            }
+
+            Directory.CreateDirectory(outputDirectory);
+
+            foreach (var extension in extensions)
+            {
+                Directory.CreateDirectory($"{outputDirectory}/{extension}s");
+            }
+
+            string[] directories = Directory.GetDirectories(outputDirectory);
+
+            foreach (var currentDirectory in directories)
+            {
+                string folderExtension = currentDirectory.Split('\\').Reverse().ToArray()[0].TrimEnd('s');
+
+                foreach (var file in files)
+                {
+                    FileInfo currentFileInfo = new FileInfo(file);
+
+                    string fileName = currentFileInfo.Name;
+                    string currentExtension = currentFileInfo.Extension.TrimStart('.');
+
+                    if (currentExtension == folderExtension)
+                    {
+                        File.Copy(file, $"{currentDirectory}/{fileName}");
+                    }
+                }
+            }
+        }
+    }
+}
