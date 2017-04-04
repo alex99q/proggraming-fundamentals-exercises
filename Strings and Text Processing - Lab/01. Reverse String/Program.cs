10fc8ce91f6ec0fc01fbb4cf9928f17d9f589130
alex99q <alex99q@abv.bg>
2017-04-04 11:42:51 +0300

    Added some more exercise programs

---
+++ Strings and Text Processing - Lab/01. Reverse String/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _01.Reverse_String
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string reversedInput = string.Empty;
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                reversedInput = string.Concat(reversedInput, input[i].ToString());
+            }
+
+            Console.WriteLine(reversedInput);
+        }
+    }
+}
