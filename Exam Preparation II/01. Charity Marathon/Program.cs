094cfa3cff1a3ba17a9ad2b64eb652837595c21a
alex99q <alex99q@abv.bg>
2017-04-18 00:36:32 +0300

    Added some more exercise programs

---
+++ Exam Preparation II/01. Charity Marathon/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01.Charity_Marathon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint marathonLenght = uint.Parse(Console.ReadLine());
+            uint numberOfRunners = uint.Parse(Console.ReadLine());
+            uint averageNumberOfLaps = uint.Parse(Console.ReadLine());
+            uint trackLenght = uint.Parse(Console.ReadLine());
+            uint trackCapacity = uint.Parse(Console.ReadLine());
+            decimal moneyPerKilometer = decimal.Parse(Console.ReadLine());
+
+            if (numberOfRunners > trackCapacity * marathonLenght)
+            {
+                numberOfRunners = trackCapacity * marathonLenght;
+            }
+
+            ulong averageMetersPerRunner = (ulong)averageNumberOfLaps * trackLenght * numberOfRunners;
+            ulong averageKilometers = averageMetersPerRunner / 1000;
+            decimal MoneyRaised = averageKilometers * moneyPerKilometer;
+
+            Console.WriteLine($"Money raised: {MoneyRaised:F2}");
+        }
+    }
+}
---
+++ Exam Preparation II/04. Roli The Coder/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _04.Roli_The_Coder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(\d) #(\w+?) ((?:@\w+? )+)";
+
+            Regex inputRegex = new Regex(pattern);
+
+            var eventsDict = new Dictionary<int, Dictionary<string, List<string>>>();
+            var IdEventDict = new Dictionary<int, string>();
+
+            string input = Console.ReadLine();
+            while (input != "Time for Code")
+            {
+                Match inputMatch = inputRegex.Match((input + " "));
+
+                if (!inputMatch.Success)
+                {
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                int ID = int.Parse(inputMatch.Groups[1].ToString());
+                string eventName = inputMatch.Groups[2].ToString();
+                string unparsedParticipants = inputMatch.Groups[3].ToString().Trim();
+
+                List<string> participants = unparsedParticipants
+                    .Split(' ')
+                    .Select(n => n.TrimStart('@'))
+                    .ToList();
+
+                IdEventDict[ID] = eventName;
+
+                if (!eventsDict.ContainsKey(ID))
+                {
+                    eventsDict[ID] = new Dictionary<string, List<string>>();
+                }
+
+                if (eventsDict[ID = ])
+                {
+                    if (!eventsDict[ID].ContainsKey(eventName))
+                    {
+                        eventsDict[ID][eventName] = new List<string>();
+                    }
+
+                    foreach (var participant in participants)
+                    {
+                        if (!eventsDict[ID][eventName].Contains(participant))
+                        {
+                            eventsDict[ID][eventName].Add(participant);
+                        }
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
